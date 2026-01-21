10cdfef4ec4d6b84237052ff64aa7ebec1f07231
DEEBII <kajelahambisa2013@gmail.com>
2026-01-21 05:59:11 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,28 @@ const ASSETS = [
   "./author.jpg.JPG",
   "./karaa_karraa_logo.jpg.jpg"
 ];
+const CACHE_NAME = 'karaakarraa-v1';
+const assets = [
+  './',
+  './index.html',
+  './icon.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
